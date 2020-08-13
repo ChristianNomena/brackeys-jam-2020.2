11486c25877543389a07257045eaf747a70f7c99
ChristianNomena <christiangrzk@gmail.com>
2020-08-13 16:27:36 +0300

    New Credits Menu

--- scripts/Credits.cs
+++ scripts/Credits.cs
@@ -4,18 +4,57 @@ using System;
 public class Credits : Control
 {
 	private TextureButton backButton;
+	private TextureButton teamButton;
+	private TextureButton musicButton;
+
+	private Node2D teamNode;
+	private Node2D musicNode;
+
+	private Label titleLabel;
 
 	public override void _Ready()
 	{
 		VisualServer.SetDefaultClearColor(new Color(0, 0, 0));
 
 		this.backButton = (TextureButton)GetNode("BackButton");
+		this.teamButton = (TextureButton)GetNode("TeamButton");
+		this.musicButton = (TextureButton)GetNode("MusicButton");
 
-		this.backButton.Connect("pressed", this, nameof(OnPressedBackButton));
+		this.teamNode = (Node2D)GetNode("Team");
+		this.musicNode = (Node2D)GetNode("Music");
+
+		this.titleLabel = (Label)GetNode("TitleLabel");
+
+		this.backButton.Connect("pressed", this, nameof(OnBackButtonPressed));
+
+		this.OnTeamButtonPressed();
 	}
 
-	private void OnPressedBackButton()
+	private void OnBackButtonPressed()
 	{
 		Menu.mainMenuShow = false;
 	}
+	
+	private void OnTeamButtonPressed()
+	{
+		this.teamNode.Show();
+		this.musicNode.Hide();
+
+		this.teamButton.Hide();
+		this.musicButton.Show();
+
+		this.titleLabel.Text = "THE TEAM";
+	}
+
+	private void OnMusicButtonPressed()
+	{
+		this.teamNode.Hide();
+		this.musicNode.Show();
+
+		this.teamButton.Show();
+		this.musicButton.Hide();
+
+		this.titleLabel.Text = "MUSIC";
+	}
 }
+
